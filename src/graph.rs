2a447d4fdba79b11543d80c9e69385148b79ea81
Pavan Tumuluri <pst15635@uga.edu>
2023-03-08 13:37:49 -0500

    Create interface for directed graph
    Directed graph now ensures proper state, e.g. that a vertex exists in
    the graph before you can add an edge to it, by providing an interface to
    the adjacency list. Also makes it easier to add vertices and edges.

--- src/graph.rs
+++ src/graph.rs
@@ -1,8 +1,71 @@
 use std::collections::{HashMap, HashSet, LinkedList};
 
 pub type Vertex = usize;
-pub type AdjacencyList = HashSet<Vertex>;
-pub type DirectedGraph = HashMap<Vertex, AdjacencyList>;
+type AdjacencyList = HashSet<Vertex>;
+
+pub struct DirectedGraph {
+    adj_lists: HashMap<Vertex, AdjacencyList>,
+}
+
+impl DirectedGraph {
+    pub fn new() -> Self {
+        Self {
+            adj_lists: HashMap::new(),
+        }
+    }
+
+    /// Add the vertex to the graph, doing nothing if the vertex 
+    /// already exists.
+    pub fn add_vertex(&mut self, v: Vertex) {
+        self.adj_lists.entry(v).or_insert(HashSet::new());
+    }
+
+    pub fn add_vertices(&mut self, list: &[Vertex]) {
+        for v in list.iter() {
+            self.add_vertex(*v);
+        }
+    }
+
+    pub fn contains_vertex(&self, v: Vertex) -> bool {
+        self.adj_lists.contains_key(&v)
+    }
+    
+    /// Attempt to add an edge from vertex u to vertex v.
+    /// Return Err if either of the vertices does not exist.
+    pub fn add_edge(&mut self, u: Vertex, v: Vertex) -> Result<(), &'static str> {
+        if !self.contains_vertex(u) || !self.contains_vertex(v) {
+            return Err("one or more input vertices do not exist")
+        }
+        self.adj_lists.entry(u).and_modify(|l| { l.insert(v); });
+        Ok(())
+    }
+
+    pub fn add_edges(&mut self, list: &[(Vertex, Vertex)]) -> Result<(), &'static str> {
+        for e in list.iter() {
+            if !self.contains_vertex(e.0) || !self.contains_vertex(e.1) {
+                return Err("one or more input vertices do not exist")
+            }
+        }
+        for e in list.iter() {
+            self.adj_lists.entry(e.0).and_modify(|l| { l.insert(e.1); });
+        }
+        Ok(())
+    }
+
+    /// Return a vector of vertices in the graph at the time of calling.
+    pub fn vertices(&self) -> Vec<&Vertex> {
+        self.adj_lists.keys().collect()
+    }
+
+    pub fn edges_from(&self, v: Vertex) -> Result<Vec<&Vertex>, &'static str> {
+        let adj_list = self.adj_lists.get(&v).ok_or("vertex does not exist")?;
+        Ok(adj_list.iter().collect())
+    }
+
+    pub fn num_vertices(&self) -> usize {
+        self.adj_lists.len()
+    }
+}
 
 /// Used in depth-first and breadth-first searches to denote the progress.
 #[derive(PartialEq)]
@@ -16,13 +79,14 @@ enum Color {
 /// Returns an Err if the graph contains a cycle.
 /// Based on the algorithm presented by Cormen et al. in chapter 20 of
 /// Introduction to Algorithms (4th ed).
+
 pub fn topological_sort(graph: &DirectedGraph) -> Result<LinkedList<Vertex>, &'static str> {
-    let mut colors = HashMap::with_capacity(graph.len());
-    for v in graph.keys() {
+    let mut colors = HashMap::with_capacity(graph.num_vertices());
+    for v in graph.vertices().into_iter() {
         colors.insert(*v, Color::White);
     }
     let mut schedule = LinkedList::new();
-    for v in graph.keys() {
+    for v in graph.vertices().into_iter() {
         if *colors.get(v).unwrap() == Color::White {
             depth_first_visit(&graph, *v, &mut colors, &mut schedule);
         }
@@ -36,7 +100,7 @@ fn depth_first_visit(graph: &DirectedGraph, v: Vertex,
                          schedule: &mut LinkedList<Vertex>)
 {
     colors.insert(v, Color::Gray);
-    for u in graph.get(&v).unwrap().iter() {
+    for u in graph.edges_from(v).unwrap().into_iter() {
         if *colors.get(u).unwrap() == Color::White {
             depth_first_visit(graph, *u, colors, schedule);
         }
@@ -44,6 +108,7 @@ fn depth_first_visit(graph: &DirectedGraph, v: Vertex,
     schedule.push_front(v);
     colors.insert(v, Color::Black);
 }
+
 #[cfg(test)]
 mod tests {
     use super::*;
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,9 @@
-use std::collections::{HashSet, HashMap};
 use scheduler::graph::*;
 
 fn main() {
     let mut g = DirectedGraph::new();
-    let adj_list1 = HashSet::from([2, 8]);
-    let adj_list2 = HashSet::from([3, 8]);
-    let adj_list3 = HashSet::from([6]);
-    let adj_list4 = HashSet::from([3, 5]);
-    let adj_list5 = HashSet::from([6]);
-    let adj_list6 = HashSet::new();
-    let adj_list7 = HashSet::from([8]);
-    let adj_list8 = HashSet::new();
-    let adj_list9 = HashSet::new();
-
-    g.insert(1, adj_list1);
-    g.insert(2, adj_list2);
-    g.insert(3, adj_list3);
-    g.insert(4, adj_list4);
-    g.insert(5, adj_list5);
-    g.insert(6, adj_list6);
-    g.insert(7, adj_list7);
-    g.insert(8, adj_list8);
-    g.insert(9, adj_list9);
+    g.add_vertices(&(1..10).collect::<Vec<Vertex>>());
+    g.add_edges(&vec![(1, 2), (1, 8), (2, 3), (2, 8), (3, 6), (4, 3), (4, 5), (5, 6), (7, 8)]);
 
     let schedule = topological_sort(&g);
 
