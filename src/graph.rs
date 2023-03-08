b88479fe360f1cd03625ddb56fc73c38c57d3c54
Pavan Tumuluri <pst15635@uga.edu>
2023-03-08 12:04:53 -0500

    Implement topological sort
    Implement the graph as a simple hashmap from vertices to adjacency
    lists. Going to need a wrapper around it though, because right now it
    can be in an illegal state, i.e. a vertex can be in an adjacency list
    but not exist as a vertex with its own adjacency list. For consistency,
    a vertex that exists but has 0 outdegree should be represented as an
    entry in the hashmap with an empty adjacency list, rather than by simply
    being absent.

--- src/graph.rs
+++ src/graph.rs
@@ -1,109 +1,50 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet, LinkedList};
 
-type AdjacencyList = HashSet<usize>;
+pub type Vertex = usize;
+pub type AdjacencyList = HashSet<Vertex>;
+pub type DirectedGraph = HashMap<Vertex, AdjacencyList>;
 
-/// Directed graph implemented as an adjacency list.
-/// Each vertex is represented as an unsigned integer.
-struct DirectedGraph {
-    // A None entry indicates that the vertex does not exist.
-    // This is different from a vertex with an empty adjacency list,
-    // which *does* exist but simply has an outdegree of 0.
-    adj_lists: Vec<Option<AdjacencyList>>,
-
-    // Need to keep track of this separately because the length
-    // of the list is actually more like a capacity, since None entries
-    // will be included in it.
-    num_vertices: usize,
+/// Used in depth-first and breadth-first searches to denote the progress.
+#[derive(PartialEq)]
+enum Color {
+    White,
+    Gray,
+    Black,
 }
 
-impl DirectedGraph {
-    /// Construct a new graph with the specified initial capacity.
-    /// The graph still starts with no vertices, but using a sensible
-    /// initial capacity prevents the need for frequent resizing of the
-    /// adjacency list.
-    fn new(capacity: usize) -> Self {
-        let adj_lists = Vec::with_capacity(capacity);
-        DirectedGraph {
-            adj_lists,
-            num_vertices: 0,
+/// Returns a linear ordering of the graph's vertices using topological sort.
+/// Returns an Err if the graph contains a cycle.
+/// Based on the algorithm presented by Cormen et al. in chapter 20 of
+/// Introduction to Algorithms (4th ed).
+pub fn topological_sort(graph: &DirectedGraph) -> Result<LinkedList<Vertex>, &'static str> {
+    let mut colors = HashMap::with_capacity(graph.len());
+    for v in graph.keys() {
+        colors.insert(*v, Color::White);
+    }
+    let mut schedule = LinkedList::new();
+    for v in graph.keys() {
+        if *colors.get(v).unwrap() == Color::White {
+            depth_first_visit(&graph, *v, &mut colors, &mut schedule);
         }
     }
 
-    /// Return the number of vertices in the graph.
-    fn num_vertices(&self) -> usize {
-        self.num_vertices
-    }
-    
-    /// Attempts to add a vertex at the specified index.
-    /// Returns true if the operation succeeded and false otherwise
-    fn add_vertex(&mut self, index: usize) -> bool {
-        if index >= self.adj_lists.len() {
-            self.adj_lists.resize(index + 1, None);
-        }
-        if self.adj_lists[index] == None {
-            self.adj_lists.insert(index, Some(HashSet::new()));
-            self.num_vertices += 1;
-            true
-        } else {
-            false
-        }
-    }
-
-    /// Create a directed edge from src to dest, returning Err if at least one
-    /// of the verticies does not exist.
-    pub fn add_edge(&mut self, src: usize, dest: usize) -> Result<(), &'static str> {
-        if self.has_vertex(src) && self.has_vertex(dest) {
-            self.adj_lists[src].as_mut().unwrap().insert(dest);
-            return Ok(());
-        }
-        Err("one or more vertices do not exist")
-    }
-    
-    /// Returns true if and only if the graph contains the specified vertex.
-    fn has_vertex(&self, vertex: usize) -> bool {
-        vertex < self.adj_lists.len() && matches!(self.adj_lists[vertex], Some(_))
-    }
-
-    /// Returns true if and only if an edge exists between src and dest.
-    /// Returns an Err if either of the vertices is not in the graph.
-    pub fn has_edge(&self, src: usize, dest: usize) -> Result<bool, &'static str> {
-        if self.has_vertex(src) && self.has_vertex(dest) {
-            let list = self.adj_lists[src].as_ref().unwrap();
-            for &i in list.iter() {
-                if i == dest {
-                    return Ok(true);
-                }
-            }
-            return Ok(false);
-        }
-        Err("src and/or dest vertices do not exist")
-    }
+    Ok(schedule)
 }
 
+fn depth_first_visit(graph: &DirectedGraph, v: Vertex, 
+                         colors: &mut HashMap<Vertex, Color>, 
+                         schedule: &mut LinkedList<Vertex>)
+{
+    colors.insert(v, Color::Gray);
+    for u in graph.get(&v).unwrap().iter() {
+        if *colors.get(u).unwrap() == Color::White {
+            depth_first_visit(graph, *u, colors, schedule);
+        }
+    }
+    schedule.push_front(v);
+    colors.insert(v, Color::Black);
+}
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn graph_demo() {
-        let mut g = DirectedGraph::new(10);
-        assert_eq!(g.num_vertices(), 0);
-
-        assert!(g.add_vertex(0));
-        assert_eq!(g.num_vertices(), 1);
-
-        assert!(g.add_vertex(3));
-        assert_eq!(g.num_vertices(), 2);
-
-        assert_eq!(g.adj_lists[0].as_ref().unwrap().len(), 0);
-        assert_eq!(g.adj_lists[3].as_ref().unwrap().len(), 0);
-
-        g.add_edge(0, 3);
-        assert_eq!(g.adj_lists[0].as_ref().unwrap().len(), 1);
-        assert_eq!(g.adj_lists[3].as_ref().unwrap().len(), 0);
-
-        assert!(matches!(g.has_edge(0, 3), Ok(true)));
-        assert!(matches!(g.has_edge(3, 0), Ok(false)));
-        assert!(matches!(g.has_edge(0, 2), Err(_)));
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::collections::{HashSet, HashMap};
+use scheduler::graph::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut g = DirectedGraph::new();
+    let adj_list1 = HashSet::from([2, 8]);
+    let adj_list2 = HashSet::from([3, 8]);
+    let adj_list3 = HashSet::from([6]);
+    let adj_list4 = HashSet::from([3, 5]);
+    let adj_list5 = HashSet::from([6]);
+    let adj_list6 = HashSet::new();
+    let adj_list7 = HashSet::from([8]);
+    let adj_list8 = HashSet::new();
+    let adj_list9 = HashSet::new();
+
+    g.insert(1, adj_list1);
+    g.insert(2, adj_list2);
+    g.insert(3, adj_list3);
+    g.insert(4, adj_list4);
+    g.insert(5, adj_list5);
+    g.insert(6, adj_list6);
+    g.insert(7, adj_list7);
+    g.insert(8, adj_list8);
+    g.insert(9, adj_list9);
+
+    let schedule = topological_sort(&g);
+
+    println!("{:?}", schedule);
 }
